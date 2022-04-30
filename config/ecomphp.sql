45bbbef48fd1ca4c69c6664f5e96b378ce163878
Adil Marghady <68113808+adilmarghadi1@users.noreply.github.com>
2022-04-30 21:31:41 +0000

    Update ecomphp.sql

--- config/ecomphp.sql
+++ config/ecomphp.sql
@@ -3,9 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: May 20, 2020 at 10:12 AM
--- Server version: 10.4.6-MariaDB
--- PHP Version: 7.3.8
+
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 SET AUTOCOMMIT = 0;
